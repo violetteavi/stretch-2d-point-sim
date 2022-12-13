c174546786c789921b36255730b85bf5f9ba9403
David Gundana <49731085+davidgundana@users.noreply.github.com>
2022-12-12 16:01:28 -0800

    point_sim: Dropping keys, reset capabilities

--- stretch_2D_point_emulator.py
+++ stretch_2D_point_emulator.py
@@ -5,8 +5,8 @@ robot = stretch_body.robot.Robot()
 robot.startup()
 
 def stop_robot():
-    robot.end_of_arm.move_to('wrist_yaw', 0)
-    robot.end_of_arm.move_to('stretch_gripper', 60)
+    #robot.end_of_arm.move_to('wrist_yaw', 0)
+    #robot.end_of_arm.move_to('stretch_gripper', 60)
     robot.lift.set_velocity(v_m=0)
     robot.arm.set_velocity(v_m=0)
     robot.push_command()
@@ -50,17 +50,39 @@ def compute_velocity_swirl(px, py):
     vx = (1-swirl_coefficient)*vx_center + swirl_coefficient*vx_swirl
     vy = (1-swirl_coefficient)*vy_center + swirl_coefficient*vy_swirl
     return vx, vy
-    
-if __name__ == "__main__":
-    stop_robot()
-    robot.arm.move_to(0.2)
-    robot.lift.move_to(0.5)
+
+def move_to_blocking(x, y):
+    robot.arm.move_to(x)
+    robot.lift.move_to(y)
     robot.push_command()
     time.sleep(5)
+
+def drop_keys():
+    #move_to_blocking(0.3, 0.56)
+    move_to_blocking(0.15, 0.56)
+    move_to_blocking(0.15, 0.86)
+    move_to_blocking(0.3, 0.86)
+    robot.end_of_arm.move_to('stretch_gripper', 60)
+    robot.push_command()
+    time.sleep(5)
+
+def reset():
+    #move_to_blocking(0.3, 0.86)
+    move_to_blocking(0.15, 0.86)
+    move_to_blocking(0.15, 0.56)
+    move_to_blocking(0.3, 0.56)
+    robot.end_of_arm.move_to('stretch_gripper', 0)
+    robot.push_command()
+    time.sleep(5)
+
+if __name__ == "__main__":
+    stop_robot()
+    reset()
+    #drop_keys()
     # update new position
     robot.push_command()
     loop_count = 0
-    while True:
+    while False:
         px, py = get_position()
         vx, vy = compute_velocity_swirl(px, py)
         send_velocity(vx, vy)
